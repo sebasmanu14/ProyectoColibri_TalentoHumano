5eb8cd73a57c9968bd56217f98eef53f923ce32d
alvaroisraelc26 <aim.cruz@yavirac.edu.ec>
2023-01-31 00:51:37 -0500

    Loginstyle

--- includes/login.php
+++ includes/login.php
@@ -1,50 +1,34 @@
-
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>login</title>
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link rel="stylesheet" href="css/stylelogin.css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	
-
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>	
 </head>
 
 <body>
-<form  action="_functions.php" method="POST">
-<div id="login" >
-        <div class="container">
-            <div id="login-row" class="row justify-content-center align-items-center">
-                <div id="login-column" class="col-md-6">
-                    <div id="login-box" class="col-md-12">
-                        <br>
-           
-                   <br>
-                                    <h3 class="text-center">Iniciar Sesión</h3>
-                       <br>
-                            <div class="form-group">
-                                <label for="correo">Usuario:</label><br>
-                                <input type="text" name="nombre" id="nombre" class="form-control" required>
-                            </div>
-                            <div class="form-group">
-                                <label for="password">Contraseña:</label><br>
-                                <input type="password" name="password" id="password" class="form-control" required>
-                                <input type="hidden" name="accion" value="acceso_user">
-                            </div>
-                            <div class="form-group">
-                             <br>
-                    <center>
-                                <input type="submit"class="btn btn-success" value="Ingresar">   
-                                </center>
-                        </form>
-                    </div>
-                </div>
-            </div>
+    <div class="loginbox">
+        <div id="login" >      
+            <img src="css/img/usu.webp" alt="" class="avatar">
+                <h1>Login</h1>
+                <br>
+                <form  action="_functions.php" method="POST">
+                    <p>Usuario</p>
+                    <input type="text" name="nombre" id="nombre" class="form-control" placeholder="Usuario" required>
+                    <p>Contraseña</p>
+                    <input type="password" name="password" id="password" class="form-control" placeholder="Contraseña" required>
+                    <input type="hidden" name="accion" value="acceso_user">
+                    <input type="submit" value="Iniciar Sesión">
+                    <a href="#">Olvido su contraseña</a><br>
+                    <a href="#">No tiene una cuenta?</a>        
+                </form>
         </div>
     </div>
-    </form>
 </body>
+
 </html>
